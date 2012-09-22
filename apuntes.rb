68f1bbb4e8207ac71bd309bff92142654bcf8d96
Jose Daniel <jdcuadrado3@gmail.com>
2012-09-22 19:04:34 +0100

    Practica 1 SWT terminada.

---
+++ apuntes.rb
@@ -0,0 +1,11 @@
+<html>
+	<head><title> ROCK, paper, scissors </title>
+	<link set ="stylesheet" type="text/css"
+		href= "/style.css" />
+	</head>
+	<body>
+		<%= yield %>
+		<form ....
+		</form>
+	</body>
+</html>
